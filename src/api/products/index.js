9a288889d319dc18692407815fcbc891e2c60829
Riccardo Gulin <riccardo@nucleode.com>
2022-07-25 12:00:33 +0200

    Adds product image upload

--- src/api/products/index.js
+++ src/api/products/index.js
@@ -1,6 +1,9 @@
 import express from "express"
 import createHttpError from "http-errors"
+import multer from "multer"
+import { extname } from "path"
 import { findProductById, findProductByIdAndDelete, findProductByIdAndUpdate, findProducts, saveNewProduct } from "../../lib/db/products.js"
+import { saveProductsImages } from "../../lib/fs/tools.js"
 import { checksProductsSchema, checkValidationResult } from "./productsValidation.js"
 
 const productsRouter = express.Router()
@@ -53,6 +56,28 @@ productsRouter.delete("/:productId", async (req, res, next) => {
   try {
     await findProductByIdAndDelete(req.params.productId)
     res.status(204).send()
+  } catch (error) {
+    next(error)
+  }
+})
+
+productsRouter.patch("/:productId/image", multer().single("productPicture"), async (req, res, next) => {
+  try {
+    // create a unique name for that picture (name will be something like 3kgarsl60hymte.gif)
+
+    const fileName = req.params.productId + extname(req.file.originalname)
+
+    // save the file into public folder
+
+    // update the product record with the image url
+    const product = await findProductByIdAndUpdate(req.params.productId, { imageUrl: "/img/products/" + fileName })
+    if (product) {
+      await saveProductsImages(fileName, req.file.buffer)
+      // send back a response
+      res.send(product)
+    } else {
+      next(createHttpError(404, `Product with id ${req.params.productId} not found!`))
+    }
   } catch (error) {
     next(error)
   }
--- src/lib/fs/tools.js
+++ src/lib/fs/tools.js
@@ -2,9 +2,11 @@ import fs from "fs-extra"
 import { dirname, join } from "path"
 import { fileURLToPath } from "url"
 
-const { readJSON, writeJSON } = fs
+const { readJSON, writeJSON, writeFile } = fs
 
 const productsJSONPath = join(dirname(fileURLToPath(import.meta.url)), "../../data/products.json")
+const publicProductsFolderPath = join(process.cwd(), "./public/img/products")
 
 export const getProducts = () => readJSON(productsJSONPath)
 export const writeProducts = productsArray => writeJSON(productsJSONPath, productsArray)
+export const saveProductsImages = (fileName, file) => writeFile(join(publicProductsFolderPath, fileName), file)
